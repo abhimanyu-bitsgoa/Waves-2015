bafd38d3c929e8b738ef6ff2afb45e079b12cd2e
Abhimanyu Singh <abhimanyu.bitsgoa@gmail.com>
2015-10-03 22:50:30 +0530

    Added basic sample tabs to the project

---
+++ Waves/app/src/main/java/bits/mobileappclub/waves/Tab1.java
@@ -0,0 +1,23 @@
+package bits.mobileappclub.waves;
+
+/**
+ * Created by Aronzxxx on 03-10-2015.
+ */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Edwin on 15/02/2015.
+ */
+public class Tab1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab_1,container,false);
+        return v;
+    }
+}
