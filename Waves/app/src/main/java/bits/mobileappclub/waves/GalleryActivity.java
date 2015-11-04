8966ce43b8436bff0e5b062548167b86eace6e37
Mudit18 <mudit.d7869@gmail.com>
2015-11-04 18:07:21 +0530

    Implemented Picasso for Gallery.

--- Waves/app/src/main/java/bits/mobileappclub/waves/GalleryActivity.java
+++ Waves/app/src/main/java/bits/mobileappclub/waves/GalleryActivity.java
@@ -1,5 +1,6 @@
 package bits.mobileappclub.waves;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,7 +8,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 public class GalleryActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
@@ -18,14 +27,25 @@ public class GalleryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gallery);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ProgressDialog dialog=ProgressDialog.show(GalleryActivity.this,"Loading","Please wait");
+        ParseQuery<ParseObject> parseQuery=ParseQuery.getQuery("Gallery");
+        try {
+            List<ParseObject> gallery=parseQuery.find();
+            int[] imageResourceId = { R.drawable.waves,R.drawable.salim, R.drawable.tvf, R.drawable.aking};
+            Log.d("Size of gallery",String.valueOf(gallery.size()));
+            mAdapter=new RVAdapter(gallery,GalleryActivity.this);
+            dialog.dismiss();
+        } catch (ParseException e) {
+            e.printStackTrace();
+            dialog.dismiss();
+            //TODO make a slack to show internet not available
+        }
         RecyclerView rv = (RecyclerView)findViewById(R.id.rv);
         rv.setHasFixedSize(true);
         LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
         rv.setLayoutManager(llm);
-        int[] imageResourceId = { R.drawable.waves,R.drawable.salim, R.drawable.tvf, R.drawable.aking};
-        mAdapter=new RVAdapter(imageResourceId);
         rv.setAdapter(mAdapter);
-
 
     }
 
